c0102790623013fce862f9d02f00a7cebb43f757
Spencer Mandrusiak <spmandrus@icloud.com>
2024-10-25 20:12:17 -0600

    commit

--- custom_components/hasc/sensor.py
+++ custom_components/hasc/sensor.py
@@ -45,6 +45,8 @@ async def async_setup_entry(
     password = config["password"]
     coordinator = MyThermostatApiClientCoordinator(hass, session, username, password)
     await coordinator.async_config_entry_first_refresh()
+    _LOGGER.debug("CHECK THERMOSTATUS")
+    _LOGGER.debug("%s", len(coordinator.api.thermostats))
     for thermostat in coordinator.api.thermostats:
         async_add_entities(
             [
