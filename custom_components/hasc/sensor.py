79dbde8d66992d47a5aab8121350f159bb1f8f6f
Spencer Mandrusiak <spmandrus@icloud.com>
2024-10-28 21:25:50 -0600

    idk

--- custom_components/hasc/sensor.py
+++ custom_components/hasc/sensor.py
@@ -45,6 +45,7 @@ async def async_setup_entry(
     password = config["password"]
     coordinator = ThermostatCoordinator(hass, session, username, password)
     await coordinator.async_config_entry_first_refresh()
+    adds = []
     for thermostat in coordinator.api.thermostats:
         entities = []
         for energy_type in EnergyCalculationDuration:
@@ -58,8 +59,8 @@ async def async_setup_entry(
                         energy_type
                     ),
             )
-
-        async_add_entities(entities)
+        adds.append(entities)
+    async_add_entities(adds)
 
 
 class EnergyCalculationDuration(Enum):
